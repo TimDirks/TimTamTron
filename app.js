2057f1d85bcf60cbf6a400006a4d9c53fbf88c0a
Unknown <tim.dirks@hotmail.com>
2018-06-26 19:34:46 +0200

    Keeping Heroku awake

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var port = process.env.PORT || 5000;
 var app = express();
+var http = require('http');
 
 // Mongoose database setup
 var dbConfig = require('./config/database');
@@ -14,8 +15,16 @@ require('./models/user.js');
 
 var client = require('./bot.js');
 
+app.listen(port, function(){
+    console.log("Server started ..! on port " + port);
+});
+
 client.login(config.token);
 
-app.listen(port, function(){
-    console.log("Server started ..!");
+// Keep Heroku awake
+app.use('/', function (req, res){
+    res.send("I'm awake!");
 });
+setInterval(function() {
+    http.get("https://timtamtron.herokuapp.com/");
+}, 600000);
