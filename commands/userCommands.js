5e988a3fc69cbd79aea814628c26c34967ff1fad
Unknown <tim.dirks@hotmail.com>
2018-06-30 13:08:36 +0200

    Added Rock Paper Scissors and more stats

--- commands/userCommands.js
+++ commands/userCommands.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 User = mongoose.model('User');
 Guild = mongoose.model('Guild');
 
+var rps = [ "rock", "paper", "scissors"];
+var rpsBeat = { rock:"scissors", paper:"rock", scissors:"paper"};
+
 var switchCmd = function getCommand(message){
     const args = message.content.slice(this.prefix.length).trim().split(' ');
     const cmd = args.shift().toLowerCase();
@@ -25,6 +28,9 @@ var switchCmd = function getCommand(message){
         case "joke":
             getJoke(message);
             break;
+        case "rps":
+            playRps(message, args);
+            break;
     }
 };
 
@@ -36,6 +42,7 @@ function getHelp(message){
     help += "\n**t.joke -** Will crack a funny joke.";
     help += "\n**t.copy [message] -** Will repeat what you said and delete your message.";
     help += "\n**t.stats [user] -** Get some statistics of yourself or someone else.";
+    help += "\n**t.rps [rock/paper/scissors] -** Play a game of Rock Paper Scissors against the bot.";
     help += "\n\nFor admin commands use **t.admin [command]**";
     help += "\n\n```For any more information look for TimTam :)```";
 
@@ -62,6 +69,12 @@ function complimentUser(message){
     User.findOneOrCreate({ userId: member.id }, function (err, user) {
         if (err) return console.log(err);
         user.complimented += 1;
+        user.save();
+    });
+
+    User.findOneOrCreate({ userId: message.author.id }, function (err, user) {
+        if (err) return console.log(err);
+        user.complimenting += 1;
         user.save();
     });
 
@@ -100,6 +113,37 @@ function getJoke(message){
     });
 }
 
+function playRps(message, args){
+    if(args.length === 0 || rps.indexOf(args[0].toLowerCase()) <= -1) return message.channel.send("Please give a valid option to play (Rock, Paper, Scissors).");
+    var play = args[0].toLowerCase();
+    var botChoice = rps[Math.floor(Math.random() * rps.length)];
+
+    var result = "I choose "+ botChoice +"! ";
+    User.findOneOrCreate({ userId: message.author.id }, function (err, user) {
+        if (err) return console.log(err);
+        User.findOneOrCreate({ userId: client.user.id }, function (err, bot) {
+            if (err) return console.log(err);
+
+        if(play === botChoice){
+            user.rpsTie += 1;
+            bot.rpsTie += 1;
+            result += "It's a tie! What a tough match that was.";
+        } else if(rpsBeat[play] === botChoice){
+            user.rpsWin += 1;
+            bot.rpsLose += 1;
+            result += "You win! Nicely played!";
+        } else {
+            user.rpsLose += 1;
+            bot.rpsWin += 1;
+            result += "You Lose! Perhaps you'll win next time.";
+        }
+        bot.save();
+        user.save();
+        message.channel.send(result);
+        });
+    });
+}
+
 function copyMessage(message, args){
     const copyMessage = args.join(' ');
     message.delete().catch(function(){
@@ -116,8 +160,10 @@ function getStats(message){
         if (err) return console.log(err);
         //Add more stats when there are more.
         var stats = "**Statistics for "+member+":**";
-        stats += "\nComplimented: "+user.complimented+" times";
+        stats += "\nGot a Compliment: "+user.complimented+" times";
+        stats += "\nGave a Compliment: "+user.complimenting+" times";
         stats += "\nCracked a joke: "+user.joked+" times";
+        stats += "\nRock Paper Sciccors wins/ties/loses: "+user.rpsWin+"/"+user.rpsTie+"/"+user.rpsLose;
         message.channel.send(stats);
     });
 }
